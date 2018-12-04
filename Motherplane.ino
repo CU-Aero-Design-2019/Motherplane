23d4b993382bcf0448fe9b09632d91170c8f249b
Spencer Graffunder <spencergraffunder@cedarforest.cedarville.edu>
2018-12-04 18:35:48 -0500

    stuff

--- Motherplane.ino
+++ Motherplane.ino
@@ -13,6 +13,7 @@
 #include <SpecMPU6050.h>
 #include <SpecRFD900.h>
 #include <SpecBMP180.h>
+#include <SpecHMC5883.h>
 
 SpecBMP180 bmp;
 
@@ -20,23 +21,25 @@ SpecBMP180 bmp;
 String telemetry = "";
 
 void setup(){
+    // load settings from EEPROM
+    Settings::loadSettings();
+
     // GPS setup
     SpecGPS::setup();
 
     // USB serial setup
     USB::setup();
 
+    SpecHMC5883::setup();
+
     // IMU setup
     SpecMPU6050::setup();
 
-    delay(2000);
-
-    // load settings from EEPROM
-    Settings::loadSettings();
-
+    delay(1000);
+    
     SpecRFD900::setup(&Serial3);
 
-    SpecSD::setup();
+    SpecSD::setup("test002.txt");
 
     if (!bmp.begin()) {
         Serial.println("Could not find a valid BMP085 sensor");
@@ -47,6 +50,8 @@ void loop(){
     // check for incoming serial data
     USB::update();
     SpecRFD900::update();
+
+    // needs to be constantly updated
     SpecGPS::update();
     
     if(millis() - SpecMPU6050::UpdateTimer > 1000/SpecMPU6050::UpdatePeriod){
@@ -55,43 +60,84 @@ void loop(){
         SpecMPU6050::UpdateTimer = millis();
     }
 
+    if(millis() - SpecHMC5883::UpdateTimer > 1000/SpecHMC5883::UpdatePeriod){
+        SpecHMC5883::update();
+        SpecHMC5883::UpdateTimer = millis();
+        //Serial.println(SpecHMC5883::x);
+    }
+
     if(millis() - SpecRFD900::UpdateTimer > 1000/SpecRFD900::UpdatePeriod){
         telemetry = "";
 
-        // add current time
-        telemetry += SpecGPS::gps.time.value();
-        telemetry += " ";
+        // // add current time
+        // telemetry += SpecGPS::gps.time.value();
+        // telemetry += " ";
 
-        // speed
-        telemetry += SpecGPS::gps.speed.value();
-        telemetry += " ";
+        // // speed
+        // telemetry += SpecGPS::gps.speed.value();
+        // telemetry += " ";
 
-        // location
-        telemetry += SpecGPS::gps.location.lat();
-        telemetry += " ";
-        telemetry += SpecGPS::gps.location.lng();
-        telemetry += " ";
+        // // location
+        // telemetry += SpecGPS::gps.location.lat();
+        // telemetry += " ";
+        // telemetry += SpecGPS::gps.location.lng();
+        // telemetry += " ";
 
-        // add altitude
-        telemetry += bmp.readOffsetAltitude();
-        telemetry += " ";
+        // // add altitude
+        // telemetry += bmp.readOffsetAltitude();
+        // telemetry += " ";
         
-        // add angleX
-        telemetry += SpecMPU6050::angleX;
-        telemetry += " ";
+        // // add angleX
+        // telemetry += SpecMPU6050::angleX;
+        // telemetry += " ";
 
-        telemetry += millis()/1000;
+        // telemetry += millis()/1000;
+        // telemetry += " ";
+
+        telemetry += SpecGPS::gps.location.lat(); // deg
+        telemetry += " ";
+        telemetry += SpecGPS::gps.location.lng(); // deg
+        telemetry += " ";
+        telemetry += SpecGPS::gps.speed.mps(); // m/s
+        telemetry += " ";
+        telemetry += SpecGPS::gps.altitude.value(); // cm
+        telemetry += " ";
+        telemetry += bmp.readOffsetAltitude(); // m
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleAccX; // deg
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleAccY;
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleAccZ;
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleGyroX; // deg/s
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleGyroY;
+        telemetry += " ";
+        telemetry += SpecMPU6050::angleGyroZ;
+        telemetry += " ";
+        telemetry += SpecHMC5883::x; // ?
+        telemetry += " ";
+        telemetry += SpecHMC5883::y;
+        telemetry += " ";
+        telemetry += SpecHMC5883::z;
+        telemetry += " ";
+        telemetry += SpecHMC5883::heading; // deg
+        telemetry += " ";
+        telemetry += millis()/100; // ds
         telemetry += " ";
 
         telemetry += "!";
         SpecRFD900::sendTelemetry(telemetry);
-        Serial.println(telemetry);
+        SpecSD::writeTelemetry(telemetry);
         //Serial.println("sending telemetry");
+        Serial.println(telemetry);
         SpecRFD900::UpdateTimer = millis();
+
     }
 
     if(millis() - SpecSD::UpdateTimer > 1000/SpecSD::UpdatePeriod){
-        SpecSD::writeTelemetry(telemetry);
+        Serial.println(telemetry);
         SpecSD::UpdateTimer = millis();
     }
 
--- constants.h
+++ constants.h
@@ -2,7 +2,7 @@
 #define CONSTANTS_H
 
 const int serialDelay = 10;
-const int USBSerialBaudrate = 9600;
+const int USBSerialBaudrate = 250000;
 const int GPSSerialBaudrate = 9600;
 const int ReceiverSerialBaudrate = 115200;
 
