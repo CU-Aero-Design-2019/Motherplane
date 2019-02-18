5a8358910242df979a24d1813a7b9b98d789b31d
Spencer Graffunder <spencergraffunder@cedarforest.cedarville.edu>
2019-02-18 17:05:08 -0500

    add compass back in

--- Motherplane.ino
+++ Motherplane.ino
@@ -22,6 +22,7 @@ Updates fast enough
 #include <SpecMPU6050.h>
 #include "SpecRFD900.h"
 #include <SpecBMP180.h>
+#include <SpecHMC5883.h>
 #ifdef MEMORYCHECK
 	#include <MemoryFree.h>;
 #endif
@@ -65,7 +66,9 @@ void setup() {
 	
 	// USB serial setup
 	USB::setup();
-
+	
+	SpecHMC5883::setup();
+	
     // IMU setup
     //SpecMPU6050::setup();
 			
@@ -111,6 +114,8 @@ void loop() {
     SpecRFD900::update();
 	
 	USB::update();
+	
+	SpecHMC5883::update();
 	
 	// update drop values
 	Drop::update();
@@ -237,7 +242,7 @@ void loop() {
 		telemetry += String(Drop::sendBack, HEX);
         telemetry += " ";
 		
-		telemetry += Prediction::bearing;
+		telemetry += SpecHMC5883::heading;
 		telemetry += " ";
 		
         telemetry += "!";
